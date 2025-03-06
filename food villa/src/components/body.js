be53bfd3ecf07aa9564960ba73d4e6c41ecc8838
Lakshay <lakshay2876@gmail.com>
2025-03-06 18:31:46 +0530

    added separate modules and search bar

--- food villa/src/components/body.js
+++ food villa/src/components/body.js
@@ -1,16 +1,51 @@
 import RestaurantCards from "./RestaurantCard";
 import { RestaurantList } from "../config";
+import { useState } from "react";
+
+function filterData(searchText, restaurantsArray) {
+  if (!searchText) return restaurantsArray;
+  const data = restaurantsArray.filter((restaurant) =>
+    restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return data;
+}
 
 const Body = () => {
-    return (
-        <div className="restaurant-list">
-            {
-                RestaurantList.map(restaurant => {
-                    return <RestaurantCards {...restaurant.info}/>
-                })
-            }
-        </div>
-    );
-}
+  const [restaurantsArray, setRestaurantsArray] = useState(RestaurantList);
+  const [filteredRestaurantsArray, setfilteredRestaurantsArray] = useState(RestaurantList);
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          id="search-bar"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <button
+          id="search-btn"
+          onClick={() => {
+            //to filter the restaurants based on the searched text
+            const data = filterData(searchText, restaurantsArray);
+            //display the restaurants cards
+            setfilteredRestaurantsArray(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {filteredRestaurantsArray.map((restaurant) => {
+          return <RestaurantCards {...restaurant.info} />;
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Body;
--- food villa/src/config.js
+++ food villa/src/config.js
@@ -1562,3 +1562,4 @@ export function cuisine_items (items) {
 }
 
 export const IMG_CDN = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVoZjD7O31QdLKhNwoD57fdVsxRxgDL2rG3g&s";
+
