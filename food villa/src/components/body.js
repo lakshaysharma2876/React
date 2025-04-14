22fe8e33ed229e9a47a7362fc671c6557a9f7e16
Lakshay <lakshay2876@gmail.com>
2025-04-15 00:21:58 +0530

    15 April 2025

--- food villa/src/components/body.js
+++ food villa/src/components/body.js
@@ -17,16 +17,16 @@ const Body = () => {
   const [filteredRestaurantsArray, setfilteredRestaurantsArray] = useState(RestaurantList);
   const [searchText, setSearchText] = useState("");
 
-  // useEffect(() => {
-  //   getRestaurants();
-  // }, []); 
+  useEffect(() => {
+    getRestaurants();
+  }, []); 
   
 
-  // async function getRestaurants() {
-  //   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
-  //   const json = await data.json();
-  //   setRestaurantsArray(json);
-  // }
+  async function getRestaurants() {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    setRestaurantsArray(json);
+  }
 
   //if you want more data use hard-coded data
 
