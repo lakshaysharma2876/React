403e84573543ec4e776dfa2c7bb06b49afd073a6
Lakshay <lakshay2876@gmail.com>
2025-04-19 15:17:55 +0530

    19/04/25

---
+++ food villa/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+        <h1>This is About Us Page</h1>
+        <p> I am learning React Paths</p>
+        </div>
+    )
+}
+
+export default About;
--- food villa/src/components/ShimmerUI.js
+++ food villa/src/components/ShimmerUI.js
@@ -1,24 +1,17 @@
 const ShimmerUI = () => {
-    console.log("shimmer loading");
-    return (
-      <>
-        <div className="search-container-shimmer"></div>
-        <div className="restaurant-list-shimmer">
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-          <div className="cards-shimmer"> </div>
-        </div>
-      </>
-    );
+  console.log("shimmer loading");
+  return (
+    <>
+      <div className="search-container-shimmer"></div>
+      <div className="restaurant-list-shimmer">
+        {Array(12)
+          .fill("")
+          .map((e, index) => (
+            <div key={index} className="cards-shimmer"> </div>
+          ))}
+      </div>
+    </>
+  );
 };
 
 export default ShimmerUI;
