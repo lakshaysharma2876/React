e95ea6868eec781a07b30f44a8b54dc48ae9fed1
Lakshay <lakshay2876@gmail.com>
2025-06-10 20:21:14 +0530

    Routing

--- food villa/src/food_app.js
+++ food villa/src/food_app.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider,} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import ReactDOM from "react-dom/client";
 import Header from "./components/header";
 import Body from "./components/body";
@@ -9,38 +9,40 @@ import Cart from "./components/Cart";
 import Error from "./components/Error";
 
 const FinalLayout = () => {
-    return (
-        <>
-        <Header/>
-        <Body/>
-        <Footer/>
-        </>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
 
 const appRouter = createBrowserRouter([
-    {
-        path : "/",
-        element : <FinalLayout />,
-        errorElement : <Error />
-    },
-    {
-        path : "/about",
-        element : <About />
-    },
-    {
-        path : "/home",
-        element : <FinalLayout />
-    },
-    {
-        path : "/ContactUs",
-        element : <ContactUs />
-    },
-    {
-        path : "/Cart",
-        element : <Cart />
-    }
+  {
+    path: "/",
+    element: <FinalLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/home",
+        element: <FinalLayout />,
+      },
+      {
+        path: "/ContactUs",
+        element: <ContactUs />,
+      },
+      {
+        path: "/Cart",
+        element: <Cart />,
+      },
+    ],
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
