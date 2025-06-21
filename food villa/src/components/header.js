e3deb5318f452879889a38ce18d374632677c46f
Lakshay <lakshay2876@gmail.com>
2025-06-21 18:30:49 +0530

    tailwindcss

--- food villa/src/components/header.js
+++ food villa/src/components/header.js
@@ -3,15 +3,15 @@ import { IMG_CDN } from "../config";
 
 const Header = () => {
     return (
-        <div className="header" >
+        <div className="flex flex-wrap justify-between bg-white " >
             <a href="/">
-            <img id="food_icon_image" src={IMG_CDN} alt ="Food Villa icon"/>
+            <img id="food_icon_image" src={IMG_CDN} alt ="Food Villa icon" className="size-30 p-2.5" />
             </a>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contactUs">Contact Us</Link></li>
-                <li><Link to="/cart">Cart</Link></li>
+            <ul className="flex flex-wrap p-3 content-between font-bold">
+                <li className="py-5 px-3 hover:text-red-500"><Link to="/">🏠Home</Link></li>
+                <li className="py-5 px-3 hover:text-red-500"><Link to="/about">🅰️About</Link></li>
+                <li className="py-5 px-3 hover:text-red-500"><Link to="/contactUs">©️Contact Us</Link></li>
+                <li className="py-5 px-3 hover:text-red-500"><Link to="/cart">🛒Cart</Link></li>
             </ul>
         </div>
     );
