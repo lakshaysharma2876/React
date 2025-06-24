88414661d59ca0d5ad341537cd3325261cd44016
Lakshay <lakshay2876@gmail.com>
2025-06-24 22:36:51 +0530

    Tailwind CSS

--- food villa/src/components/ShimmerUI.js
+++ food villa/src/components/ShimmerUI.js
@@ -2,12 +2,11 @@ const ShimmerUI = () => {
   console.log("shimmer loading");
   return (
     <>
-      <div className="search-container-shimmer"></div>
-      <div className="restaurant-list-shimmer">
+      <div className="flex flex-wrap py-24 gap-2">
         {Array(12)
           .fill("")
           .map((e, index) => (
-            <div key={index} className="cards-shimmer"> </div>
+            <div key={index} className="border-1 border-amber-50 bg-gray-100 w-72 h-72 rounded-md"> </div>
           ))}
       </div>
     </>
